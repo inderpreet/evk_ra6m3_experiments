4a7330103e5834ea7fbb171fe6bc779f49453df7
Inderpreet Singh <er.inderpreet@gmail.com>
2023-12-27 21:16:57 -0500

    Code cleanup and add comments.

--- src/app.c
+++ src/app.c
@@ -1,85 +1,154 @@
-/*
- * app.c
+/******************************************************************************
+ * @file app.c
  *
- *  Created on: Dec. 27, 2023
- *      Author: Ip_v1
- */
+ * @brief Application for testing Development Kit
+ *
+ * NOTES:
+ *
+ * @author Inderpreet Singh(ip_v1)
+ *
+ *****************************************************************************/
+
+/******************************************************************************
+ * Global Includes
+ *****************************************************************************/
 #include "hal_data.h"
 #include "app.h"
 
+/******************************************************************************
+ * Global Variable Declarations
+ *****************************************************************************/
 volatile uint32_t g_systick_counter = 0;
 extern bsp_leds_t g_bsp_leds;
-
 volatile const uint8_t str1[] = "Hello, World\n\r"; // 14 len
-//    const uint8_t index = 0;
 
-inline void mon_init(void)
-{
-    R_SCI_UART_Open (&uart_one_ctrl, &uart_one_cfg);
-    R_AGT_Open (&g_timer0_ctrl, &g_timer0_cfg);
-    R_AGT_Start (&g_timer0_ctrl);
+/******************************************************************************
+ *
+ * FUNCTION NAME: mon_init
+ *
+ * DESCRIPTION:
+ * Initialization code for the test application.
+ *
+ * CREATED BY:
+ * Inderpreet Singh
+ *
+ * PARAMETERS:  None.
+ *
+ * RETURN VALUE:  None.
+ *
+ * Notes:  None.
+ *
+ *****************************************************************************/
+inline void mon_init(void) {
+	R_SCI_UART_Open(&uart_one_ctrl, &uart_one_cfg);
+	R_AGT_Open(&g_timer0_ctrl, &g_timer0_cfg);
+	R_AGT_Start(&g_timer0_ctrl);
 }
 
-void mon_send(void)
-{
-    R_SCI_UART_Write (&uart_one_ctrl, (uint8_t*) str1, 14);
+/******************************************************************************
+ *
+ * FUNCTION NAME: mon_send
+ *
+ * DESCRIPTION:
+ * Function to send out a fixed string over USART
+ *
+ * CREATED BY:
+ * Inderpreet Singh
+ *
+ * PARAMETERS:  None.
+ *
+ * RETURN VALUE:  None.
+ *
+ * Notes:  None.
+ *
+ *****************************************************************************/
+void mon_send(void) {
+	R_SCI_UART_Write(&uart_one_ctrl, (uint8_t*) str1, 14);
 }
 
-void tmr0_callback(timer_callback_args_t *p_args)
-{
-    static uint8_t level = 0;
+/******************************************************************************
+ *
+ * FUNCTION NAME: tmr0_callback
+ *
+ * DESCRIPTION:
+ * - Callback function for Timer 0. Function is called every 1ms as
+ * 		per setting in BSP/FSP. Global variable g_systick_counter is incremented
+ * 		everytime there is a call.
+ * - Also toggles GPIO at P207 at 1ms
+ *
+ * CREATED BY:
+ * Inderpreet Singh
+ *
+ * PARAMETERS:
+ *
+ * RETURN VALUE:  None.
+ *
+ * Notes:  None.
+ *
+ *****************************************************************************/
+void tmr0_callback(timer_callback_args_t *p_args) {
+	static uint8_t level = 0;
 //    __NOP();
-    g_systick_counter++;
+	g_systick_counter++;
 
-    // Toggle GPIO P207
-    R_IOPORT_PinWrite (&g_ioport_ctrl, BSP_IO_PORT_02_PIN_07, level);
-    if (level == 0)
-    {
-        level = 1;
-    }
-    else
-    {
-        level = 0;
-    }
+	// Toggle GPIO P207
+	R_IOPORT_PinWrite(&g_ioport_ctrl, BSP_IO_PORT_02_PIN_07, level);
+	if (level == 0) {
+		level = 1;
+	} else {
+		level = 0;
+	}
 }
 
-void task1(void)
-{
-    volatile static uint8_t st = 0;
-    bsp_leds_t leds = g_bsp_leds;
-    static bsp_io_level_t pin_level = BSP_IO_LEVEL_LOW;
+/******************************************************************************
+ *
+ * FUNCTION NAME: task1
+ *
+ * DESCRIPTION:
+ * - Simple task to blink the BSP LEDs every 500ms. Task monitors systick counter
+ * 		and toggled all LEDs.
+ * - Also send out Hello World by calling mon_send()
+ *
+ * CREATED BY:
+ * Inderpreet Singh
+ *
+ * PARAMETERS:  None.
+ *
+ * RETURN VALUE:  None.
+ *
+ * Notes:  None.
+ *
+ *****************************************************************************/
+void task1(void) {
+	bsp_leds_t leds = g_bsp_leds;
+	static bsp_io_level_t pin_level = BSP_IO_LEVEL_LOW;
 
-    static uint32_t cnt = 0;
-    uint32_t temp = g_systick_counter;
+	static uint32_t cnt = 0;
+	uint32_t temp = g_systick_counter;
 
-    if (temp - cnt > 100)
-    {
-        cnt = temp;
-        R_BSP_PinAccessEnable ();
+	if (temp - cnt > 500) {
+		cnt = temp;
+		R_BSP_PinAccessEnable();
 
-        /* Update all board LEDs */
-        for (uint32_t i = 0; i < leds.led_count; i++)
-        {
-            /* Get pin to toggle */
-            uint32_t pin = leds.p_leds[i];
+		/* Update all board LEDs */
+		for (uint32_t i = 0; i < leds.led_count; i++) {
+			/* Get pin to toggle */
+			uint32_t pin = leds.p_leds[i];
 
-            /* Write to this pin */
-            R_BSP_PinWrite ((bsp_io_port_pin_t) pin, pin_level);
-        }
+			/* Write to this pin */
+			R_BSP_PinWrite((bsp_io_port_pin_t) pin, pin_level);
+		}
 
-        /* Protect PFS registers */
-        R_BSP_PinAccessDisable ();
+		/* Protect PFS registers */
+		R_BSP_PinAccessDisable();
 
-        mon_send ();
+		/* Toggle level for next write */
+		if (BSP_IO_LEVEL_LOW == pin_level) {
+			pin_level = BSP_IO_LEVEL_HIGH;
+		} else {
+			pin_level = BSP_IO_LEVEL_LOW;
+		}
 
-        /* Toggle level for next write */
-        if (BSP_IO_LEVEL_LOW == pin_level)
-        {
-            pin_level = BSP_IO_LEVEL_HIGH;
-        }
-        else
-        {
-            pin_level = BSP_IO_LEVEL_LOW;
-        }
-    }
+		mon_send();
+	} // end if
 }
--- src/hal_entry.c
+++ src/hal_entry.c
@@ -31,77 +31,25 @@ extern bsp_leds_t g_bsp_leds;
  * Blinks all leds at a rate of 1 second using the software delay function provided by the BSP.
  *
  **********************************************************************************************************************/
-void hal_entry (void)
-{
+void hal_entry(void) {
 #if BSP_TZ_SECURE_BUILD
-
     /* Enter non-secure code */
     R_BSP_NonSecureEnter();
 #endif
 
-    /* Define the units to be used with the software delay function */
-//    const bsp_delay_units_t bsp_delay_units = BSP_DELAY_UNITS_MILLISECONDS;
+	bsp_leds_t leds = g_bsp_leds;
 
-    /* Set the blink frequency (must be <= bsp_delay_units */
-//    const uint32_t freq_in_hz = 2;
+	if (0 == leds.led_count) {
+		while (1) {
+			;                          // There are no LEDs on this board
+		}
+	}
 
+	mon_init();
 
-    /* Calculate the delay in terms of bsp_delay_units */
-//    const uint32_t delay = bsp_delay_units / freq_in_hz;
-
-    /* LED type structure */
-    bsp_leds_t leds = g_bsp_leds;
-
-    /* If this board has no LEDs then trap here */
-    if (0 == leds.led_count)
-    {
-        while (1)
-        {
-            ;                          // There are no LEDs on this board
-        }
-    }
-
-    /* Holds level to set for pins */
-//    bsp_io_level_t pin_level = BSP_IO_LEVEL_LOW;
-
-    mon_init();
-
-    while (1)
-    {
-        /* Enable access to the PFS registers. If using r_ioport module then register protection is automatically
-         * handled. This code uses BSP IO functions to show how it is used.
-         */
-//        R_BSP_PinAccessEnable();
-
-        /* Update all board LEDs */
-//        for (uint32_t i = 0; i < leds.led_count; i++)
-//        {
-//            /* Get pin to toggle */
-//            uint32_t pin = leds.p_leds[i];
-//
-//            /* Write to this pin */
-//            R_BSP_PinWrite((bsp_io_port_pin_t) pin, pin_level);
-//        }
-
-        /* Protect PFS registers */
-//        R_BSP_PinAccessDisable();
-
-//        mon_send();
-        task1();
-
-        /* Toggle level for next write */
-//        if (BSP_IO_LEVEL_LOW == pin_level)
-//        {
-//            pin_level = BSP_IO_LEVEL_HIGH;
-//        }
-//        else
-//        {
-//            pin_level = BSP_IO_LEVEL_LOW;
-//        }
-//
-//        /* Delay */
-//        R_BSP_SoftwareDelay(delay, bsp_delay_units);
-    }
+	while (1) {
+		task1();
+	}
 }
 
 /*******************************************************************************************************************//**
@@ -110,10 +58,8 @@ void hal_entry (void)
  *
  * @param[in]  event    Where at in the start up process the code is currently at
  **********************************************************************************************************************/
-void R_BSP_WarmStart (bsp_warm_start_event_t event)
-{
-    if (BSP_WARM_START_RESET == event)
-    {
+void R_BSP_WarmStart(bsp_warm_start_event_t event) {
+	if (BSP_WARM_START_RESET == event) {
 #if BSP_FEATURE_FLASH_LP_VERSION != 0
 
         /* Enable reading from data flash. */
@@ -122,13 +68,12 @@ void R_BSP_WarmStart (bsp_warm_start_event_t event)
         /* Would normally have to wait tDSTOP(6us) for data flash recovery. Placing the enable here, before clock and
          * C runtime initialization, should negate the need for a delay since the initialization will typically take more than 6us. */
 #endif
-    }
+	}
 
-    if (BSP_WARM_START_POST_C == event)
-    {
-        /* C runtime environment and system clocks are setup. */
+	if (BSP_WARM_START_POST_C == event) {
+		/* C runtime environment and system clocks are setup. */
 
-        /* Configure pins. */
-        R_IOPORT_Open(&g_ioport_ctrl, g_ioport.p_cfg);
-    }
+		/* Configure pins. */
+		R_IOPORT_Open(&g_ioport_ctrl, g_ioport.p_cfg);
+	}
 }
